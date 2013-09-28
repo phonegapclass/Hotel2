cdb780dc9e1bacf9dd2a29852e578d8bb1d448d8
Carlos Celma <solrac@igitsoft.com>
2013-09-28 13:21:41 -0500

    d

--- js/API/servidor.js
+++ js/API/servidor.js
@@ -3,7 +3,7 @@ function enviarRegistro(nombre,email,telefono){
 	alert(nombre+email+telefono);
 	$.ajax({
 		type: "POST",
-		url: "some.php",
+		url: "http://www.igitsoft.com/pgtest.php",
 		data: { nom: "John", mai: "Boston", tel: telefono }
 	}).done(function( msg ){
 		alert( "Data Saved: " + msg );
