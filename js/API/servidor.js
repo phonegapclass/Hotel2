755a2ed88b44491eb617486583fdf01a2479f7ad
Carlos Celma <solrac@igitsoft.com>
2013-09-28 14:12:08 -0500

    poi

--- js/API/servidor.js
+++ js/API/servidor.js
@@ -3,7 +3,7 @@ function enviarRegistro(nombre,email,telefono){
 	alert(nombre+email+telefono);
 	$.ajax({
 		type: "POST",
-		url: "http://www.igitsoft.com/pgtest.php",
+		url: "http://192.168.1.69/pgtest.php",
 		data: "nom="+nombre+"&mai="+email+"&tel="+tel,
 		error: function(){
 			alert('Ajax error');	
